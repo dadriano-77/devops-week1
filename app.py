cc2d93b619a8a7ae6e3c9b197114f2f5e8932c75
dadriano-77 <dadriano2@77soft.com>
2026-05-07 17:08:49 +0800

    feat: add /version endpoint

--- app.py
+++ app.py
@@ -39,5 +39,10 @@ def db_version():
     return jsonify(version=version)
 
 
+@app.route("/version")
+def version():
+    return "1.0.0"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
